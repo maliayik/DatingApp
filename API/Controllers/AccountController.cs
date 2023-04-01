cd542cc148f0f3ce90973cdc3b75cdbd873e2ece
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-04-01 01:17:07 +0100

    Adding Login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -25,7 +25,7 @@ namespace API.Controllers
             if (await UserExist(registerDto.Username)) return BadRequest("Username is taken");
 
 
-                using var hmac = new HMACSHA512();
+            using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
@@ -42,8 +42,33 @@ namespace API.Controllers
 
         private async Task<bool> UserExist(string username)
         {
-            return await _context.Users.AnyAsync(x=> x.UserName== username.ToLower());
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
+
+
+        [HttpPost("login")]
+
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x =>
+             x.UserName == loginDto.Username);
+
+            if (user == null )
+            return Unauthorized("invalid username");
+
+            using var hmac = new HMACSHA512();
+
+            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i=0; i<computeHash.Length; i++)
+            {
+                if(computeHash[i] !=user.PasswordHash[i])
+                return Unauthorized("invalid password");
+            }
+
+            return user;
+        }
+
 
     }
 }

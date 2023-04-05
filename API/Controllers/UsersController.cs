caa24563e633f6daada00dabdc4085fc31d222d1
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-04-05 11:15:15 +0100

    Adding Authentication

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-   
+    [Authorize]
+
     public class UsersController : BaseApiController
     {
 
@@ -16,6 +18,8 @@ namespace API.Controllers
             _context = context;
 
         }
+
+        [AllowAnonymous]
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()

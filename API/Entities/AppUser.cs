be7576f1a74aa525d23956c62b69431069980a66
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-04-21 17:39:12 +0100

    Adding DateTime Extension to calculate age

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,5 @@
+using API.Extension;
+
 namespace API.Entities
 {
     public class AppUser
@@ -8,16 +10,20 @@ namespace API.Entities
         public byte[] PasswordSalt { get; set; }
         public DateOnly DateOfBirth { get; set; }
         public string KnownAs { get; set; }
-        public DateTime Created { get; set; }=DateTime.UtcNow;
-        public DateTime LastActive { get; set; }=DateTime.UtcNow;
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interest { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public List<Photo> Photos { get; set; }=new();
+        public List<Photo> Photos { get; set; } = new();
 
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
 
     }
 }

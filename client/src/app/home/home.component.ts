4366e524934178b857f17718ae1f0c10ad0f4121
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-04-24 17:24:27 +0100

    retrieving the list of members

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +10,10 @@ export class HomeComponent implements OnInit {
   registerMode =false;
   users:any;
 
-  constructor(private http:HttpClient){}
+  constructor(){}
 
-  ngOnInit(): void {
-    
-    this.getUsers();
+  ngOnInit(): void {    
+
   }
 
   registerToggle(){
@@ -23,16 +21,7 @@ export class HomeComponent implements OnInit {
     this.registerMode=!this.registerMode
   }
 
-  getUsers() {
-
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-
-    })
-  }
-
+ 
   cancelRegisterMode(event:boolean){
     this.registerMode=event;
   }

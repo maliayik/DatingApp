9fa0b1a907e3ec26a79277280f23bf204909eeff
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-04-27 12:16:03 +0100

    update user in the client app

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -13,10 +13,10 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
-  @ViewChild('editForm') editForm : NgForm | undefined;
-  @HostListener('window:beforeunload',['$event']) unloadNotification($event:any){
-    if(this.editForm?.dirty){
-      $event.returnValue=true;
+  @ViewChild('editForm') editForm: NgForm | undefined;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
     }
   }
   member: Member | undefined;
@@ -42,13 +42,13 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateMember() {
-    console.log(this.member);
-    this.toastr.success('Profile updated successfully');
-    this.editForm?.reset(this.member);
+    this.memberService.updateMember(this.editForm?.value).subscribe({
+      next: _ => {
+        this.toastr.success('Profile updated successfully');
+        this.editForm?.reset(this.member);
 
+      }
+    });
   }
-
-
-
 
 }

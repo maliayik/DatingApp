c1638829a827046ea2d5512c16947cde5c837266
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-04-29 12:47:02 +0100

    adding photo uploader

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -5,6 +5,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { FileUploadModule } from 'ng2-file-upload';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     NgxGalleryModule,
     NgxSpinnerModule.forRoot({
       type:'line-scale-party'
-    })
+    }),
+    FileUploadModule,
   ],
   exports: [
     BsDropdownModule,
@@ -27,6 +29,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     TabsModule,
     NgxGalleryModule,
     NgxSpinnerModule,
+    FileUploadModule,
   ]
 })
 export class SharedModule { }

0231a9c2ac47c2190a2190211d72e9dfe7468608
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-05-14 21:17:34 +0100

    adding filtering to API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -34,6 +34,15 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<PagedList<MemberDto>>> GetUser([FromQuery]UserParams userParams)
         {
+            var currentUser = await _userRepository.GetUserByUsernameAsync(User.GetUserName());
+            userParams.CurrentUsername=currentUser.UserName;
+
+            if(string.IsNullOrEmpty(userParams.Gender))
+            {
+                userParams.Gender = currentUser.Gender == "male" ? "female" : "male";
+            }
+
+
             var users = await _userRepository.GetMembersAsync(userParams);
 
             Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage,users.PageSize,users.TotalCount,users.TotalPage));

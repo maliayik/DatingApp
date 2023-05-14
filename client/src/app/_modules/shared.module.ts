ba205dc0d4f153957d08ac4c3e6e3699cb8cf0a8
Mehmet Ali AYIK <mehmetaliayikk@gmail.com>
2023-05-14 19:12:25 +0100

    using angular bootstrap pagination component

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -7,6 +7,7 @@ import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { FileUploadModule } from 'ng2-file-upload';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 
 
@@ -25,6 +26,7 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     }),
     FileUploadModule,
     BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
@@ -34,6 +36,7 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
     NgxSpinnerModule,
     FileUploadModule,
     BsDatepickerModule,
+    PaginationModule,
   ]
 })
 export class SharedModule { }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -36,5 +36,13 @@ export class MemberListComponent implements OnInit {
     })
   }
 
+  pageChanged(event:any){
+    if(this.pageNumber !== event.page)
+    {
+      this.pageNumber=event.page;
+      this.loadMembers();
+    }    
+  }
+
 
 }

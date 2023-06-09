13470056cb969f538ad8c58acfd48845fba87563
maliiayik <maliiayik@gmail.com>
2023-06-09 23:11:10 +0300

    Using query params

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -13,7 +13,7 @@ import { MessageService } from 'src/app/_services/message.service';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
-  @ViewChild('memberTabs') memberTabs? : TabsetComponent;
+  @ViewChild('memberTabs', {static:true}) memberTabs? : TabsetComponent;
   member: Member | undefined;
   galleryOptions: NgxGalleryOptions[] = []
   galleryImages: NgxGalleryImage[] = []
@@ -27,6 +27,14 @@ export class MemberDetailComponent implements OnInit {
   }
   ngOnInit(): void {
     this.loadMember();
+
+    this.route.queryParams.subscribe({
+      next:params=>{
+        params['tab'] && this.selectTab (params['tab'])
+      }
+    })
+
+
     this.galleryOptions = [
       {
         width: '500px',
@@ -64,6 +72,12 @@ export class MemberDetailComponent implements OnInit {
       }
     })
   }
+
+  selectTab(heading:string){
+    if(this.memberTabs)
+    this.memberTabs.tabs.find(x=> x.heading === heading)!.active=true
+  }
+  
 
   loadMessages(){
     if(this.member){
